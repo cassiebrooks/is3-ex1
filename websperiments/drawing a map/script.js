2c0f5d35fdc0a960ee966802e8e93386f0787a09
hoodscott <scottdothood@gmail.com>
2014-11-22 16:12:15 +0000

    Experiment to draw the svg map
    Created a new folder because I didn't want to break anything.

---
+++ websperiments/drawing a map/script.js
@@ -0,0 +1,33 @@
+var width = 960,
+    height = 1160;
+	
+var projection = d3.geo.albers()
+	.center([0, 55.4])
+	.rotate([4.4, 0])
+	.parallels([40, 60])
+	.scale(6000)
+	.translate([width / 2, height / 2]);
+	
+var path = d3.geo.path()
+	.projection(projection);
+
+var svg = d3.select("body").append("svg")
+    .attr("width", width)
+    .attr("height", height);
+		
+d3.json("sco.json", function(sco) {
+	svg.selectAll(".lad")
+		.data(topojson.feature(sco, sco.objects.lad).features)
+		.enter().append("path")
+		.attr("class", function(d) { return "subunit " + d.id; })
+		.attr("d", path)
+		.append("svg:title")
+		.text(function(d) { return d.properties.LAD13NM; });
+		
+	svg.append("path")
+      .datum(topojson.mesh(sco, sco.objects.lad, function(a, b) { return a !== b; }))
+      .attr("d", path)
+      .attr("class", "subunit-boundary");
+		}
+
+	);
